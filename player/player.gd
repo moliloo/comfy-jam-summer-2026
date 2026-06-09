2e66431fa3d0e0a5e85d144f7e25ef3b881873eb
LoafSab <Spider-Miles@outlook.com>
2026-06-09 23:46:47 +0100

    imported bubble sprites, renamed some things and changed inputs

--- player/player.gd
+++ player/player.gd
@@ -10,22 +10,22 @@ func _ready() -> void:
 	if GlobalVariables.get_player_position():
 		set_global_position(GlobalVariables.get_player_position());
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	move_player()
 
 func move_player() -> void:
-	if Input.is_action_pressed('move_right'):
+	if Input.is_action_pressed('move_left'):
 		sprite.flip_h = false;
 		direction.x = 1;
-	elif Input.is_action_pressed('move_left'):
+	elif Input.is_action_pressed('move_right'):
 		sprite.flip_h = true;
 		direction.x = -1;
 	else: 
 		direction.x = 0;
 
-	if Input.is_action_pressed('move_up'):
+	if Input.is_action_pressed('move_down'):
 		direction.y = -1;
-	elif Input.is_action_pressed('move_down'):
+	elif Input.is_action_pressed('move_up'):
 		direction.y = 1;
 	else: 
 		direction.y = 0;
