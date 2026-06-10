1785d5d994fd953d8c279ca21d2289230370bd44
Murilo Brito <91566541+moliloo@users.noreply.github.com>
2026-06-10 00:17:52 -0300

    Add aquarium sprites and fix player movement directions (#1)
    * imported aquarium sprite
    
    * imported bubble sprites, renamed some things and changed inputs
    
    ---------
    
    Co-authored-by: LoafSab <Spider-Miles@outlook.com>

--- cute_horror/aquarium/script.gd
+++ cute_horror/aquarium/script.gd
@@ -8,7 +8,7 @@ func _physics_process(delta: float) -> void:
 	if can_interact:
 		if Input.is_action_just_pressed('interact'):
 			GlobalVariables.set_player_position(entity.get_global_position());
-			get_tree().change_scene_to_file('res://cute_horror/tamagotchi/scene.tscn');
+			get_tree().change_scene_to_file('res://cute_horror/tamagotchi/tamagotchi.tscn');
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body as Player == null: return;
