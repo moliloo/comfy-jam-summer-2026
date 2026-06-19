cec1848c3f3f5488874bd3b161271c66ff0514fb
Murilo Brito <dev.murilobrito@gmail.com>
2026-06-19 01:51:31 -0300

    feat: add spawnpoints and transition areas in beach scene

---
+++ scenes/beach/beach.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+var entity: Player;
+var spawn_point: String;
+
+var area_entered: String;
+var areas_list: Dictionary = {
+	'house': ['res://scenes/house/house.tscn', 'FromBeach'],
+};
+
+
+func _physics_process(delta: float) -> void:
+	if area_entered:
+		if Input.is_action_just_pressed('interact'):
+			ChangeScenes.change_scene(area_entered, entity, spawn_point);
+
+
+func _on_house_door_area_body_entered(body: Node2D) -> void:
+	if body as Player == null: return;
+	entity = body;
+	area_entered = areas_list['house'][0];
+	spawn_point = areas_list['house'][1];
+
+
+func _on_house_door_area_body_exited(body: Node2D) -> void:
+	if body as Player == null: return
+	entity = null;
+	area_entered = '';
